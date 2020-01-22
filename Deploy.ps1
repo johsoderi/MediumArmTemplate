4e35281b9090c20de31993fd4abbdf59f65c2be6
johsoderi <johannes.soderberg.eriksson@gmail.com>
2020-01-22 15:07:37 +0100

    add public ip

--- Deploy.ps1
+++ Deploy.ps1
@@ -19,7 +19,6 @@ if ([string]::IsNullOrWhiteSpace($adminUsername)) { $adminUsername = "serveradmi
 
 #$adminPassword = Read-Host -Prompt ("Choose a password for " + $adminUsername) -AsSecureString
 $generatedPw = [System.Web.Security.Membership]::GeneratePassword(25,10)
-#$generatedPw = "testpw"
 $adminPassword = ConvertTo-SecureString -String $generatedPw -AsPlainText -Force
 Write-Output ("A secure password for '" + $adminUsername + "' was generated and will be stored in the following Azure Key Vault: : " + $rg + "/" + $rg + "Vault")
 

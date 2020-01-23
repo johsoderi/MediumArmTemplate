efe7d2c6f1063dff844f2cd9dad3c86dadaca8a0
johsoderi <johannes.soderberg.eriksson@gmail.com>
2020-01-23 14:30:45 +0100

    Fetch html docs from blob cont. + Cleanup

--- apache_webServer1.sh
+++ apache_webServer1.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
+
 apt-get -y update
-
-# install Apache2
 apt-get -y install apache2 
-apt-get -y install mysql-server
-apt-get -y install php libapache2-mod-php php-mysql
-# write some HTML
-echo \<center\>\<h1\>Testar om de funkar\</h1\>\<br/\>\</center\> > /var/www/html/test.html
-
-# restart Apache
+curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server1.html > /var/www/html/index.html
 apachectl restart

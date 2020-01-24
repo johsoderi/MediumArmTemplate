900c7ed620c42a22a9dda60e87372414961899ef
johsoderi <johannes.soderberg.eriksson@gmail.com>
2020-01-24 10:57:43 +0100

    FIx bash syntax

--- apache_webServer1.sh
+++ apache_webServer1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get -y update 2>&1 | curl -d qr.ax
-apt-get -y upgrade 2>&1 | curl -d qr.ax
-apt-get -y install apache2  2>&1 | curl -d qr.ax
-curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server1.html > /var/www/html/index.html 2>&1 | curl -d qr.ax
-apachectl restart 2>&1 | curl -d qr.ax
+apt-get -y update 2>&1 | curl -d @- qr.ax
+apt-get -y upgrade 2>&1 | curl -d @- qr.ax
+apt-get -y install apache2 2>&1 | curl -d @- qr.ax
+curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server1.html > /var/www/html/index.html 2>&1 | curl -d @- qr.ax
+apachectl restart 2>&1 | curl -d @- qr.ax
 

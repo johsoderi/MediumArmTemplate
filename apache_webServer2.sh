1e1013a32370c59b211176e186af84f96267c929
johsoderi <johannes.soderberg.eriksson@gmail.com>
2020-01-24 13:16:15 +0100

    Edit scripts

--- apache_webServer2.sh
+++ apache_webServer2.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-# All lines are echo'ed and curl posted to a live-tracking web server.
+# Comments and most cmd's are echo'ed and curl posted to a live-tracking web server.
 # I'm sure there are better ways to track the progress of this script, but it works.
 
-echo "wget --no-cache -O - https://gist.github.com/EmpireWorld/737fbb9f403d4dd66dee1364d866ba7e/raw/install-lamp.sh | bash" 2>&1 | curl -d @- http://qr.ax
-wget --no-cache -O - https://gist.github.com/EmpireWorld/737fbb9f403d4dd66dee1364d866ba7e/raw/install-lamp.sh | bash 2>&1 | curl -d @- http://qr.ax
+echo "Update Package Index" 2>&1 | curl -d @- http://qr.ax
+sudo apt update 2>&1 | curl -d @- http://qr.ax
 
-: <<'END'
-# Trying the LAMP stack install script above instead!
+echo "Install Apache2, MySQL, PHP" 2>&1 | curl -d @- http://qr.ax
+sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli 2>&1 | curl -d @- http://qr.ax
 
-echo "# curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server1.html > /var/www/html/index.html" 2>&1 | curl -d @- qr.ax
-curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server1.html > /var/www/html/index.html 2>&1 | curl -d @- qr.ax
+echo "Allow to run Apache on boot up" 2>&1 | curl -d @- http://qr.ax
+sudo systemctl enable apache2 2>&1 | curl -d @- http://qr.ax
 
-echo "# apt-get -y update" 2>&1 | curl -d @- qr.ax
-apt-get -y update 2>&1 | curl -d @- qr.ax
+echo "Restart Apache Web Server" 2>&1 | curl -d @- http://qr.ax
+sudo systemctl start apache2 2>&1 | curl -d @- http://qr.ax
 
-echo "# apt-get -y install apache2" 2>&1 | curl -d @- qr.ax
-apt-get -y install apache2 2>&1 | curl -d @- qr.ax
+echo "Adjust Firewall" 2>&1 | curl -d @- http://qr.ax
+sudo ufw allow in "Apache Full" 2>&1 | curl -d @- http://qr.ax
 
-echo "# apachectl restart" 2>&1 | curl -d @- qr.ax
-apachectl restart 2>&1 | curl -d @- qr.ax
-END
+echo "Allow Read/Write for Owner" 2>&1 | curl -d @- http://qr.ax
+sudo chmod -R 0755 /var/www/html/ 2>&1 | curl -d @- http://qr.ax
+
+echo "Create info.php for testing php processing" 2>&1 | curl -d @- http://qr.ax
+sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
+echo "Copy index.html" 2>&1 | curl -d @- http://qr.ax
+curl https://joso1801storage.blob.core.windows.net/joso1801blobs/server2.html > /var/www/html/index.html
+
+echo "Done." 2>&1 | curl -d @- http://qr.ax
